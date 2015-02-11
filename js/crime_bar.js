6f959df9554adfb54dc2350fa3033a56a2a4ed49
MaximMVM <maksim1981m@gmail.com>
2015-02-11 18:09:45 +0300

    update

---
+++ js/crime_bar.js
@@ -0,0 +1,84 @@
+var progress = document.getElementById('progress_b');
+var max_p = progress.max;
+var crime_progress = document.getElementById('crime_val');
+var step = document.getElementById('step');
+var step_t = document.getElementById('step_t');
+var crime_twee = document.getElementById("crimes_twee");
+var id_inc, id_dec;
+var auto_cotton = document.getElementById("auto_crime_chb");
+
+if (localStorage["crimes.progress"] == undefined || NaN || null) localStorage["crimes.progress"] = '0';
+
+
+var changeProgress = function() {
+    progress.value += 1;
+    step.value = step.max;
+    step_t.value = step_t.max;
+    localStorage["crimes.progress1"] = progress.value;
+    crime_twee.href = 'https://twitter.com/intent/tweet?text=Прокачал свою крымнашесть на  ' + localStorage["crimes.progress"] + ' и ' + progress.value + ' крымнаша';
+
+    id_dec = setInterval('stepProgress()', 250);
+    
+    if (progress.value == progress.max) {
+        localStorage["crimes.progress1"] = 0;
+        localStorage["crimes.progress"] ++;
+        progress.value = 0;
+    }
+    
+};
+
+var stepProgress = function() {
+
+    if (step.value == 0) {
+        clearInterval(id_dec);
+    }
+    step.value -= 10; 
+ 
+    step_t.value -= 12;
+    
+};
+
+
+$('#rus').dblclick(function (){
+    $('#link').css('display','none');
+    $('#quote').css('color', '#660000');
+    $('#quote').text('ВОУ ВОУ, КУДА ТАК СПЕШИШЬ? Полехче. Подожди чуть-чуть');
+    var trig_l = setInterval('vou()', 40);
+        
+});
+
+var vou = function(){
+    if (step.value == 0) { 
+        $('#link').css('display','block');
+        $('#quote').css('color','#000');
+        clearInterval(this);
+    };
+    
+};
+
+$('#auto_c').click(function(){
+    window.auto_cr = setInterval("$.fn.cotton();", 2000);
+    $(this).css('display', 'none');
+    $('#pcb1').css('display', 'none');
+    $('#cb1').css('display', 'none');
+    $('#stop_c').css('display','block');
+});
+
+$('#stop_c').click(function(){
+    clearInterval(window.auto_cr);
+    $('#auto_c').css('display', 'block');
+    $('#cb1').css('display', 'block');
+    $('#pcb1').css('display', 'block');
+});
+
+
+
+var textValColor = function(){
+    $('#textvalue').css('color', '#660000');
+    setTimeout(function(){$('#textvalue').css('color', '#000')}, 600);
+};
+
+
+
+
+
---
+++ js/datacrime.js
@@ -0,0 +1,81 @@
+localStorage["backup.progress"] = localStorage["crimes.progress"];
+localStorage["backup.progress1"] = localStorage["crimes.progress1"];
+var crime_val = document.getElementById('progress_b');
+var small_v = document.getElementById('small_value');
+var big_val = document.getElementById('crime_val');
+var set_win_st = document.getElementById('setting_window');
+
+        if (crime_val.value == 0) {
+                crime_val.value = Number(localStorage["crimes.progress1"]);
+             };
+
+        
+
+ if (small_v.innerHTML == '0') small_v.innerHTML = localStorage["crimes.progress1"];
+ if (big_val.innerHTML == '0') big_val.innerHTML = localStorage["crimes.progress"];
+ 
+ 
+ $('#close').click(function(){
+     $('#setting_window').css('z-index', '0');
+     $('#setting_window').fadeOut(250);
+     
+
+ });
+$('#setting').click(function(){
+    if (set_win_st.style.display == 'block') $('#setting_window').fadeOut(250);
+    $('#setting_window').css('display', 'block');
+    $('#setting_window').css('z-index', '10');
+    
+    
+});
+ 
+ $('#clear_data').click(function(){
+        if ((localStorage["crimes.progress1"] && localStorage["crimes.progress1"])!= '0') {
+         localStorage["backup.progress"] = localStorage["crimes.progress"];
+         localStorage["backup.progress1"] = localStorage["crimes.progress1"];
+         localStorage["crimes.progress1"] = 0;
+         localStorage["crimes.progress"] = 0;
+         progress.value = 0;
+     }
+         
+         $('#crime_val, #small_value').text('0');
+     
+ });
+ 
+ $("#beckup_data").click(function(){
+
+     localStorage["crimes.progress"]= localStorage["backup.progress"];
+     localStorage["crimes.progress1"]= localStorage["backup.progress1"];
+     progress.value = localStorage["crimes.progress1"];
+     $('#small_value').text(localStorage["crimes.progress1"]);
+     $('#crime_val').text(localStorage["crimes.progress"]);
+ });
+ 
+ $('#remove_data').click(function(){
+    $('#must_no').css('display', 'block');
+    $('#overlay').css('display', 'block');
+    $('#must_no').css('z-index', '20');
+ });
+ 
+ $('#overlay, #close_must_no, #cansel').click(function(){
+     $('#must_no').css('display', 'none');
+    $('#overlay').css('display', 'none');
+     
+ });
+ $('#ok').click(function() {
+     localStorage["backup.progress"] = localStorage["crimes.progress"] = 0;
+     localStorage["backup.progress1"] = localStorage["crimes.progress1"] = 0;
+     progress.value = 0;
+     $('#small_value').text(localStorage["crimes.progress1"]);
+     $('#crime_val').text(localStorage["crimes.progress"]);
+     //$('#close_must').('Данные удалены, крымнашесть равна нулю');
+     //$('#no_remove').hide("slow");
+     
+     $('#must_no').hide("slow");
+     $('#data_cl').show(1000);
+     
+     setTimeout("$('#overlay, #data_cl').css('display','none');", 1500);
+     
+     
+ });
+ 
