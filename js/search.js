500d732c6119dd7c947c6a9d85124e7809e08448
MaximMVM <maksim1981m@gmail.com>
2015-02-14 04:23:31 +0300

    selection text in tweet

--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-
-
-
+$(document).ready(function()
+{   
+        
 $('#search_f').keydown(function(event){
     if(event.keyCode==13) $.fn.searchQuote();
 });
@@ -24,28 +23,46 @@ $('#search_f').keydown(function(event){
         
         for (var t1 in filtered){
              var str = '<span class = "reg">'+ qwe +'</span>';
-            $('#quote').append('<br>' + t1 + ') '+ filtered[t1].replace(qwe, str, 'gi') + ' <br>');
+            $('#quote').append('<span class = "quote_s">' + t1 + ') '+ filtered[t1].replace(qwe, str, 'gi') + '</span> <br>');
             
             
         };
      $('#quote').append('Вхождений ' + filtered.length);
+     //selection text
+
+      $('.quote_s').mouseup(function(){
+          
+      buff = window.getSelection().toString();
+  });
+      $('.quote_s').click(function(){
+        var windowOptions =   'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
+         width_t = 550;
+         height_t = 420;
+         winHeight_t = screen.height;
+         winWidth_t = screen.width;
+         left = Math.round((winWidth_t / 2) - (width_t / 2));
+         top = 0;
+        if (winHeight_t > height_t) {
+          top = Math.round((winHeight_t / 2) - (height_t / 2)+90);
+        }
+        
+        var href_win = 'https://twitter.com/intent/tweet?text=' + buff;
+        if(buff) {
+        
+            window.open(href_win, 'intent', windowOptions + ',width=' + width_t +
+                                           ',height=' + height_t + ',left=' + left + ',top=' + top);
+                                           
+            window.__twitterIntentHandler = true;
+        
+         }
+    
+        
+     
+        });
+     
      
      
  };
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
