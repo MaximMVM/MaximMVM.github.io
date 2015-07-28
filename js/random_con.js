cc15697f0d2a74c69245e711020a89dc03a77ea7
MaximMVM <maksim1981m@gmail.com>
2015-07-28 23:13:13 +0300

    рандомные объеденения слов

---
+++ js/random_con.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var mm = x.length;
+
+x[random1]; из основного масива
+x[random2];
+x[random3];
+
+str.split(/ /)[random2]; слово из массива
+
